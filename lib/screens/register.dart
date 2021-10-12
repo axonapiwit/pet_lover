9f11d34b4818e374b36f0f244f6a35aa566cc95e
apiwit <axon.apiwit@gmail.com>
2021-10-12 09:19:26 +0700

    fix login and register page

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -33,138 +33,137 @@ class _RegisterState extends State<Register> {
           }
           if (snapshot.connectionState == ConnectionState.done) {
             return Scaffold(
-              appBar: AppBar(
-                title: Text('Register'),
-              ),
-              body: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Container(
-                  child: Form(
-                    key: formKey,
-                    child: Center(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            child: Image.asset(
-                              'assets/images/logo.png',
-                              height: 150,
-                              fit: BoxFit.cover,
+              body: SingleChildScrollView(
+                child: SafeArea(
+                  child: Container(
+                    height: MediaQuery.of(context).size.height,
+                    child: Form(
+                      key: formKey,
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              child: Image.asset(
+                                'assets/images/logo.png',
+                                height: 150,
+                                fit: BoxFit.cover,
+                              ),
                             ),
-                          ),
-                          Padding(
-                              padding: EdgeInsets.all(10),
-                              child: Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.pink.shade300,
-                                    borderRadius:
-                                        new BorderRadius.circular(10.0),
+                            Padding(
+                                padding: EdgeInsets.all(10),
+                                child: Container(
+                                    decoration: BoxDecoration(
+                                      color: Colors.pink.shade300,
+                                      borderRadius:
+                                          new BorderRadius.circular(10.0),
+                                    ),
+                                    child: Padding(
+                                        padding: EdgeInsets.only(
+                                            left: 15, right: 15, top: 5),
+                                        child: TextFormField(
+                                            validator: MultiValidator([
+                                              RequiredValidator(
+                                                  errorText:
+                                                      "กรุณาป้อนอีเมลด้วย"),
+                                              EmailValidator(
+                                                  errorText:
+                                                      "รูปแบบอีเมลไม่ถูกต้อง")
+                                            ]),
+                                            keyboardType:
+                                                TextInputType.emailAddress,
+                                            onSaved: (String? email) {
+                                              profile.email = email!;
+                                            },
+                                            decoration: InputDecoration(
+                                              border: InputBorder.none,
+                                              labelText: 'Email',
+                                              labelStyle: TextStyle(
+                                                  color: Colors.white),
+                                            ))))),
+                            Padding(
+                                padding: EdgeInsets.all(10),
+                                child: Container(
+                                    decoration: BoxDecoration(
+                                      color: Colors.pink.shade300,
+                                      borderRadius:
+                                          new BorderRadius.circular(10.0),
+                                    ),
+                                    child: Padding(
+                                        padding: EdgeInsets.only(
+                                            left: 15, right: 15, top: 5),
+                                        child: TextFormField(
+                                            validator: RequiredValidator(
+                                                errorText: "กรุณาป้อนรหัสผ่าน"),
+                                            obscureText: true,
+                                            onSaved: (String? password) {
+                                              profile.password = password!;
+                                            },
+                                            decoration: InputDecoration(
+                                              border: InputBorder.none,
+                                              labelText: 'Password',
+                                              labelStyle: TextStyle(
+                                                  color: Colors.white),
+                                            ))))),
+                            SizedBox(height: 10),
+                            Center(
+                              child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Colors.pink.shade300,
                                   ),
-                                  child: Padding(
-                                      padding: EdgeInsets.only(
-                                          left: 15, right: 15, top: 5),
-                                      child: TextFormField(
-                                          validator: MultiValidator([
-                                            RequiredValidator(
-                                                errorText:
-                                                    "กรุณาป้อนอีเมลด้วย"),
-                                            EmailValidator(
-                                                errorText:
-                                                    "รูปแบบอีเมลไม่ถูกต้อง")
-                                          ]),
-                                          keyboardType:
-                                              TextInputType.emailAddress,
-                                          onSaved: (String? email) {
-                                            profile.email = email!;
-                                          },
-                                          decoration: InputDecoration(
-                                            border: InputBorder.none,
-                                            labelText: 'Email',
-                                            labelStyle:
-                                                TextStyle(color: Colors.white),
-                                          ))))),
-                          Padding(
-                              padding: EdgeInsets.all(10),
-                              child: Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.pink.shade300,
-                                    borderRadius:
-                                        new BorderRadius.circular(10.0),
-                                  ),
-                                  child: Padding(
-                                      padding: EdgeInsets.only(
-                                          left: 15, right: 15, top: 5),
-                                      child: TextFormField(
-                                          validator: RequiredValidator(
-                                              errorText: "กรุณาป้อนรหัสผ่าน"),
-                                          obscureText: true,
-                                          onSaved: (String? password) {
-                                            profile.password = password!;
-                                          },
-                                          decoration: InputDecoration(
-                                            border: InputBorder.none,
-                                            labelText: 'password',
-                                            labelStyle:
-                                                TextStyle(color: Colors.white),
-                                          ))))),
-                          SizedBox(height: 10),
-                          Center(
-                            child: ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  primary: Colors.pink.shade300,
-                                ),
-                                onPressed: () async {
-                                  if (formKey.currentState!.validate()) {
-                                    formKey.currentState!.save();
-                                    try {
-                                      await FirebaseAuth.instance
-                                          .createUserWithEmailAndPassword(
-                                              email: profile.email,
-                                              password: profile.password)
-                                          .then((value) {
-                                        DocumentReference user =
-                                            FirebaseFirestore.instance
-                                                .collection('users')
-                                                .doc(value.user!.uid);
-                                        user
-                                            .set({'isNewUser': true})
-                                            .then(
-                                                (value) => print("User Added"))
-                                            .catchError((error) => print(
-                                                "Failed to add user: $error"));
-                                        formKey.currentState!.reset();
+                                  onPressed: () async {
+                                    if (formKey.currentState!.validate()) {
+                                      formKey.currentState!.save();
+                                      try {
+                                        await FirebaseAuth.instance
+                                            .createUserWithEmailAndPassword(
+                                                email: profile.email,
+                                                password: profile.password)
+                                            .then((value) {
+                                          DocumentReference user =
+                                              FirebaseFirestore.instance
+                                                  .collection('users')
+                                                  .doc(value.user!.uid);
+                                          user
+                                              .set({'isNewUser': true})
+                                              .then((value) =>
+                                                  print("User Added"))
+                                              .catchError((error) => print(
+                                                  "Failed to add user: $error"));
+                                          formKey.currentState!.reset();
+                                          Fluttertoast.showToast(
+                                              msg:
+                                                  "สร้างบัญชีผู้ใช้เรียบร้อยแล้ว",
+                                              gravity: ToastGravity.CENTER);
+                                          // Navigator.pushReplacement(context,
+                                          //     MaterialPageRoute(builder: (context) {
+                                          //   return MainScreen();
+                                          // }));
+                                        });
+                                      } on FirebaseAuthException catch (e) {
+                                        String message;
+                                        if (e.code == 'email-already-in-use') {
+                                          message =
+                                              "อีเมลนี้ใช้งานในระบบนี้แล้ว โปรดใช้อีเมลอื่นแทน";
+                                        } else if (e.code == 'weak-password') {
+                                          message =
+                                              "รหัสผ่านต้องมีความยาวอย่างน้อย 6 ตัวอักษรขึ้นไป";
+                                        } else {
+                                          message = e.message!;
+                                        }
                                         Fluttertoast.showToast(
-                                            msg:
-                                                "สร้างบัญชีผู้ใช้เรียบร้อยแล้ว",
+                                            msg: message,
                                             gravity: ToastGravity.CENTER);
-                                        // Navigator.pushReplacement(context,
-                                        //     MaterialPageRoute(builder: (context) {
-                                        //   return MainScreen();
-                                        // }));
-                                      });
-                                    } on FirebaseAuthException catch (e) {
-                                      String message;
-                                      if (e.code == 'email-already-in-use') {
-                                        message =
-                                            "อีเมลนี้ใช้งานในระบบนี้แล้ว โปรดใช้อีเมลอื่นแทน";
-                                      } else if (e.code == 'weak-password') {
-                                        message =
-                                            "รหัสผ่านต้องมีความยาวอย่างน้อย 6 ตัวอักษรขึ้นไป";
-                                      } else {
-                                        message = e.message!;
                                       }
-                                      Fluttertoast.showToast(
-                                          msg: message,
-                                          gravity: ToastGravity.CENTER);
                                     }
-                                  }
-                                },
-                                child: Text(
-                                  'Submit',
-                                  style: TextStyle(fontSize: 18),
-                                )),
-                          )
-                        ],
+                                  },
+                                  child: Text(
+                                    'Submit',
+                                    style: TextStyle(fontSize: 18),
+                                  )),
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   ),
